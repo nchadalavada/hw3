1cbfc3bd6f61ca38feea7cdda8219271d29a393a
nchadalavada <naveen.chadalavada@kellogg.northwestern.edu>
2022-02-22 00:27:53 +0000

    commit

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,2 +1,37 @@
 class PlaceController < ApplicationController
+    def index
+        @places = Place.all
+    end
+    
+    def show
+        @place = Place.find(params["id"])
+    
+    def New
+        @place = Place.New
+    end        
+    
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/place"
+    
+    
+    def edit
+        @place = Place.find(params["id"])
+    end
+            
+            def update
+                @place= Place.find(params["id"])
+                @place.update(params["place"])
+                redirect_to "/place"
+            end
+            
+            def destroy
+                @place = Place.find(params["id"])
+                @place.destroy
+                redirect_to "/place"
+            end
+            
+            end
 end
+
